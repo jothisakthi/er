3a745884cd3e94d62c59f6fbd646cee3d219725f
jothisakthi <jothisakthivel23@gmail.com>
2016-07-31 05:56:53 -0700

    h
    j

---
+++ sum.java
@@ -0,0 +1,30 @@
+
+
+public  class sum_01{
+
+	public static void main(String args[]){
+	int a=Integer.parseInt(args[0]);
+	int b=0;
+	int sum=0;
+	int y=b;
+	//int r=0;
+	while(a!=0)
+/*{
+		r=r*10+a%10;
+		a=a/10;
+	} 
+	while(r!=0)*/
+	{
+	b=a%10;
+	sum=sum+(int)Math.pow(b,y);
+	a=a/10;
+	y=b;
+	
+
+	}
+	System.out.println(sum);
+	}
+}
+	
+
+
